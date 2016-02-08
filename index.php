9bc1ac9048922ea1d32a729fb4267493351278ac
EvgeniiShumeiko <nicesay.ru@gmail.com>
2016-02-08 19:19:56 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php $_GET["page"] ? file_exists("./page/".$_GET["page"]) ?  require './page/template.php' : require "./page/404.php" : require "./page/main.php"; 
+<?php
+/**
+ * OneLine - the most lightweight, simple and fast CMS in the world
+ * 
+ * @author EvgeniiShumeiko
+ * @version 1.0
+ */
+$_GET["page"] ? file_exists("./page/".$_GET["page"]) ?  require './page/template.php' : require "./page/404.php" : require "./page/main.php"; 
